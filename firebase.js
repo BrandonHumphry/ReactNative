bfa068e470be14f9b15a024ed6a28f291b1cd366
Brandon Humphry <brandonhumphry@gmail.com>
2021-02-14 22:51:00 -0800

    added react native dependencies and began login screen

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBX_AdWx9wOwXSi9uHGyg60fyA8RljAPXg",
+  authDomain: "signal-native-73f7a.firebaseapp.com",
+  projectId: "signal-native-73f7a",
+  storageBucket: "signal-native-73f7a.appspot.com",
+  messagingSenderId: "1081718260766",
+  appId: "1:1081718260766:web:fb19f9ac9c6ac9a1099a0f"
+};
