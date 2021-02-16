72426b9a80c2e00b66892082649f6988b3985075
Brandon Humphry <brandonhumphry@gmail.com>
2021-02-15 23:44:28 -0800

    styled login/positioning, KeyboardingAvoidingView is a sweet component

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Button, Input, Image } from "react-native-elements";
+import React, { useState } from "react";
+import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image, withTheme } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
   return (
-    <View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <StatusBar style="light" />
       <Image
         source={{
@@ -15,15 +19,41 @@ const LoginScreen = () => {
         style={{ width: 200, height: 200 }}
       />
       <View style={styles.inputContainer}>
-        <Input placeholder="Email" autoFocus type="Email" />
-        <Input placeholder="Password" secureTextEntry type="password" />
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          onChangeText={text => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          onChangeText={text => setPassword(text)}
+        />
       </View>
-    </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
   );
 };
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-  inputContainer: {}
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white"
+  },
+  inputContainer: {
+    width: 300
+  },
+  button: {
+    width: 250,
+    marginTop: 10
+  }
 });
