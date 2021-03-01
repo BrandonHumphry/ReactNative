4a39e086eed0b93f5c0f627432d60c4a104c0b0e
Brandon Humphry <brandonhumphry@gmail.com>
2021-02-28 22:27:29 -0800

    working on Home screen logging user in

---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { StyleSheet, Text, ScrollView } from "react-native";
+import CustomListItem from "../components/CustomListItem";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <CustomListItem />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
