d2bef56ab306c4d19699cefa9873715fff14118d
Brandon Humphry <brandonhumphry@gmail.com>
2021-03-15 22:39:44 -0700

    user can enter a chat and submit it, Firebase db is active and displaying chats

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,7 +1,12 @@
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Input, Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { db } from "../firebase";
 
 const AddChatScreen = ({ navigation }) => {
+  const [input, setInput] = useState("");
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Add a new chat",
@@ -9,9 +14,28 @@ const AddChatScreen = ({ navigation }) => {
     });
   }, [navigation]);
 
+  const createChat = async () => {
+    await db
+      .collection("chats")
+      .add({ chatName: input })
+      .then(() => {
+        navigation.goBack();
+      })
+      .catch(error => alert(error));
+  };
+
   return (
-    <View>
-      <Text>Add Chat</Text>
+    <View style={styles.container}>
+      <Input
+        placeholder="Enter a chat name"
+        value={input}
+        onChangeText={text => setInput(text)}
+        onSubmitEditing={createChat}
+        rightIcon={
+          <Icon name="wechat" type="antdesign" size={24} color="black"></Icon>
+        }
+      ></Input>
+      <Button onPress={createChat} title="Create new chat" />
     </View>
   );
 };
@@ -19,5 +43,9 @@ const AddChatScreen = ({ navigation }) => {
 export default AddChatScreen;
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    backgroundColor: "white",
+    padding: 30,
+    height: "100%"
+  }
 });
