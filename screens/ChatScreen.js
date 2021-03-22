291afc28dda31ff817ad869ade5601ce085f2939
Brandon Humphry <brandonhumphry@gmail.com>
2021-03-21 23:13:57 -0700

    test messages sending to db from selected message

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,12 +1,16 @@
 import React, { useLayoutEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { Avatar } from "react-native-elements";
-import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
-import { AntDesign, SimpleLineIcons, Ionicons } from "@expo/vector-icons";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { ScrollView, TouchableOpacity } from "react-native";
+import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { KeyboardAvoidingView } from "react-native";
 import { Platform } from "react-native";
+import { Keyboard } from "react-native";
+import { auth, db } from "../firebase";
+import * as firebase from "firebase";
 
 const ChatScreen = ({ navigation, route }) => {
   const [input, setInput] = useState("");
@@ -62,7 +66,19 @@ const ChatScreen = ({ navigation, route }) => {
     });
   }, [navigation]);
 
-  const sendMessage = () => {};
+  const sendMessage = () => {
+    Keyboard.dismiss();
+
+    db.collection("chats").doc(route.params.id).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      displayName: auth.currentUser.displayName,
+      email: auth.currentUser.email,
+      photoURL: auth.currentUser.photoURL
+    });
+
+    setInput("");
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
@@ -72,20 +88,23 @@ const ChatScreen = ({ navigation, route }) => {
         keyboardVerticalOffset={90}
         style={styles.container}
       >
-        <>
-          <ScrollView>{/* Chats */}</ScrollView>
-          <View style={styles.footer}>
-            <TextInput
-              value={input}
-              onChangeText={text => setInput(text)}
-              placeholder="Message"
-              style={styles.TextInput}
-            />
-            <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
-              <Ionicons name={send} size={24} color="#2B68E6" />
-            </TouchableOpacity>
-          </View>
-        </>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <>
+            <ScrollView>{/* Chats */}</ScrollView>
+            <View style={styles.footer}>
+              <TextInput
+                value={input}
+                onChangeText={text => setInput(text)}
+                onSubmitEditing={sendMessage}
+                placeholder="Message"
+                style={styles.textInput}
+              />
+              <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+                <Ionicons name="send" size={24} color="#2B68E6" />
+              </TouchableOpacity>
+            </View>
+          </>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
@@ -102,5 +121,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     padding: 15
+  },
+  textInput: {
+    bottom: 0,
+    height: 40,
+    flex: 1,
+    marginRight: 15,
+    backgroundColor: "#ECECEC",
+    padding: 10,
+    color: "grey",
+    borderRadius: 30
   }
 });
